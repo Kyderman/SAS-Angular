64779272fa741e8d20ff10c8babf348a17bd1185
Kyle Selman <kselman@mttltd.com>
2016-09-05 09:13:16 +0100

    first

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name testAppAngularApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the testAppAngularApp
+ */
+angular.module('testAppAngularApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name testAppAngularApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the testAppAngularApp
+ */
+angular.module('testAppAngularApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
